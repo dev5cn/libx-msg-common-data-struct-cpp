5f2f0306629840a74f5203ce892deec947a6ab39
xzwdev <dev5@qq.com>
2019-11-20 19:51:39 +0800

    dev5

--- pb/auto-gen.sh
+++ pb/auto-gen.sh
@@ -1,5 +1,2 @@
 #
-export LD_LIBRARY_PATH=/home/xzwdev/tools/protobuf-3.5.1/lib:$LD_LIBRARY_PATH
-export PATH=/home/xzwdev/tools/protobuf-3.5.1/bin:$PATH
-#
 protoc --proto_path=./  --cpp_out=../cpp/ ./*.proto
